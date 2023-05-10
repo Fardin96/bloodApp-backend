0e271421307ad8522c7134392dde4071d6e5d078
Fardin96 <fardinshuvro96@gmail.com>
2023-05-10 15:57:24 +0600

    connected to mongo

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ import * as dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
+const uri = process.env.DB_URI;
+const connection = mongoose.connection;
 
 app.use(cors);
 app.use(express.json());
 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(uri, { useNewUrlParser: true });
+
+connection.once("open", () => {
+  console.log("Database connection established successfully!!!");
+});
+
+connection.on("error", () => {
+  console.log("Error connecting to database!");
+});
 
 app.listen(port, () => {
   console.log("Listening on port :", port);
