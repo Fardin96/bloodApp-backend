f1553861dc8be2dc8201b73df3d72db87da1602d
Khan-17101136 <fardinshuvro96@gmail.com>
2023-05-16 21:06:06 +0600

    authentication - login

--- routes/donor.js
+++ routes/donor.js
@@ -1,24 +1,26 @@
 // import express from "express";
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 // const bcrypt = require("bcrypt");
 
 // import Donor from "../model/donor.model.js";
 const Donor = require("../model/donor.model.js");
-const { passwordHash } = require("../functions/securityFunctions.js");
+const {
+  passwordHash,
+  jwt_token,
+} = require("../functions/securityFunctions.js");
 
 // postman
-// {
-//   "name": "farabi",
-//   "email": "fardinshuvro96@gmail.com",
-//   "password": "1111",
-//   "bloodGroup": "O+",
-//   "contact": "0987655667",
-//   "address": "badda, dhaka",
-//   "dob": "12-22-23",
-//   "recency":"12-22-23",
-//   "nid": "32423492837408327"
-// }
+// // {
+// //   "name": "farabi",
+// //   "email": "fardinshuvro96@gmail.com",
+// //   "password": "1111",
+// //   "bloodGroup": "O+",
+// //   "contact": "0987655667",
+// //   "address": "badda, dhaka",
+// //   "dob": "12-22-23",
+// //   "recency":"12-22-23",
+// //   "nid": "32423492837408327"
+// // }
 
 router.route("/add").post(async (req, res) => {
   const name = req.body.name;
@@ -43,13 +45,11 @@ router.route("/add").post(async (req, res) => {
     nid,
   });
 
-  // console.log("Donor: ", newDonor);
-  // console.log("password type: ", typeof password);
-  // console.log("password: ", password);
+  const token = jwt_token(newDonor._id);
 
   newDonor
     .save()
-    .then(() => res.json("New donor added!"))
+    .then(() => res.json({ token: token }))
     .catch((err) => res.status(400).json("Error adding new donors : ", err));
 });
 
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 
 // import donorRouter from "./routes/donor.js";
 const donorRouter = require("./routes/donor");
-const Donor = require("./model/donor.model.js");
+const authentication = require("./routes/authentication");
 
 // dotenv.config();
 require("dotenv").config();
@@ -37,25 +37,7 @@ connection.on("error", () => {
 
 // defining routes using models
 app.use("/donor", donorRouter);
-
-// app.get("/", (req, res) => {
-//   // Donor.find()
-//   //   .then((donors) => res.json(donors))
-//   //   .catch((err) => res.status(400).json("Error finiding donors : ", err));
-//   console.log("how do i expect this to be called? ");
-//   res.send("Hellowww, world!");
-// });
-
-// app.post("/add", (req, res) => {
-//   // const donorName = req.body.donorName;
-//   // const newDonor = new Donor({ donorName });
-
-//   // newDonor
-//   //   .save()
-//   //   .then(() => res.json("New donor added!"))
-//   //   .catch((err) => res.status(400).json("Error adding new donors : ", err));
-//   res.send("Hellowww, world!");
-// });
+app.use("/auth", authentication);
 
 app.listen(port, () => {
   console.log("Listening on port: ", port);
